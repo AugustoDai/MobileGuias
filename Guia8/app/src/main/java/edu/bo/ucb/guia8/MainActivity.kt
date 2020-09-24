2d433de333558db9dd17e11b2c6cf8e45f75fdd6
AugustoDai <53877988+AugustoDai@users.noreply.github.com>
2020-09-24 14:57:48 -0400

    add Guia8

---
+++ Guia8/app/src/main/java/edu/bo/ucb/guia8/MainActivity.kt
@@ -0,0 +1,15 @@
+package edu.bo.ucb.guia8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Snackbar.make(layoutPrincipal , getString(R.string.mensaje_snackbar), Snackbar.LENGTH_LONG).show()
+    }
+}
